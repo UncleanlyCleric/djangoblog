384b0d87fca69f43bcb68170abd553ff258bebab
Ubuntu <py230admin@Py230-Ubtuntu02004013.qseowevrukjezjmisq4n1jj0ke.dx.internal.cloudapp.net>
2019-08-12 22:09:45 +0000

    I have no idea what's wrong

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y0(#ucw(w$u0)9324ch^^wyce%h$wgwf2r2_zrb6c0_=cyfy8^'
+SECRET_KEY = 'ns#W&y_ub%C4EDQw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
